3a6723beeba899f3afdf91a3fe2289646fbe72b4
PERSONALSOFT\setobon <setobon@personalsoft.com.co>
2021-02-01 19:10:45 -0500

    main setting and SquareCallableExample function.  3

---
+++ src/main/java/MainFutureExample.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.*;
+
+public class MainFutureExample {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        //FutureExampleMethod();
+        FutureCallableExampleMethod();
+    }
+
+    private static void FutureCallableExampleMethod() throws InterruptedException, TimeoutException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        Future<Integer> future = executor.submit(new SquareCallableExample(12));
+
+        while (!future.isDone()) {
+            System.out.println("Calculating...");
+            Thread.sleep(400);
+        }
+
+        Integer result = future.get(100, TimeUnit.SECONDS);
+        System.out.println("The result is " + result);
+
+        executor.shutdown();
+
+    }
+
+    private static void FutureExampleMethod() throws InterruptedException, ExecutionException, TimeoutException {
+        SquareFutureExample squareFutureExample = new SquareFutureExample();
+        Future<Integer> future = squareFutureExample.calculate(12);
+        Future<Integer> future1 = squareFutureExample.calculate(100);
+
+        while (!(future.isDone() && future1.isDone())) {
+
+            System.out.println("Calculating...");
+            Thread.sleep(400);
+        }
+
+        Integer result = future.get(100, TimeUnit.SECONDS);
+        Integer result1 = future1.get(100, TimeUnit.SECONDS);
+
+        System.out.println("The result is " + result);
+        System.out.println("The result1 is " + result1);
+
+        future.cancel(true);
+
+        squareFutureExample.shutDown();
+    }
+}
