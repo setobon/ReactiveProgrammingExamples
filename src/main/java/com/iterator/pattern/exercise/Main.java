c28902437567c0ff37976466e73eeb7b520218ae
PERSONALSOFT\setobon <setobon@personalsoft.com.co>
2021-02-05 09:13:14 -0500

    RxJavaTest.java -> Test methods, using Observable objects

--- src/main/java/com/iterator/pattern/exercise/Main.java
+++ src/main/java/com/iterator/pattern/exercise/Main.java
@@ -3,7 +3,7 @@ package com.iterator.pattern.exercise;
 public class Main {
     public static void main(String[] args) {
 
-        SquareRootList squareRootList = new SquareRootList(25, 100);
+        SquareRootList squareRootList = new SquareRootList(20, 1);
 
         for(double i: squareRootList){
             System.out.println(i);
--- src/test/java/com/rxjava/RxJavaTest.java
+++ src/test/java/com/rxjava/RxJavaTest.java
@@ -3,14 +3,49 @@ package com.rxjava;
 import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class RxJavaTest{
+
+    String result ="";
 
     @Test
     public void createSimpleObservable(){
-
-        String result = "";
-
         Observable<String> observable = Observable.just("Sergio");
-        //observable.subscribe((s)-> result = s);
+
+        observable.subscribe((s)-> result = s);
+
+        assertTrue(result.equals("Sergio"));
+    }
+
+    @Test
+    public void showMethodsJavaRxTest(){
+        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+        Observable<String> observable = Observable.fromArray(letters);
+
+        observable.subscribe(
+
+                (i) -> result+=i, //onNext()
+                Throwable::printStackTrace, //onError
+                ()-> result+="_Completed" //onComplete
+        );
+
+        assertTrue(result.equals("ABCDEFGHI_Completed"));
+    }
+
+    @Test
+    public void showMethodJavaRxTest2(){
+
+        String[] user = {"Sergio", "Andres", "Tobon", "Bedoya", "26", "Masculino"};
+        Observable<String> observable = Observable.fromArray(user);
+
+        observable.subscribe(
+                (value) -> result += " " + value,
+                Throwable::printStackTrace,
+                () -> result+=" _Completed"
+        );
+
+        assertEquals(result, " Sergio Andres Tobon Bedoya 26 Masculino _Completed");
     }
 }
